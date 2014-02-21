3c820a10bbc8b5902bc714038fadc32345474e03
H Berz <h9-2s@hotmail.com>
2014-02-21 12:53:20 -0500

    Forgot to copy code to the implementation page. Note to self: never have the same code written twice in two different places

--- src/sequential_kriging.cpp
+++ src/sequential_kriging.cpp
@@ -16,25 +16,46 @@ using namespace std;
 // Given a list of the initial data points this
 // function calculates the semivariance between
 // each pair and outputs it in linked-list format
-vector < Semivariance* >* calculate_all_semivariances( vector< WindVector* >& input_data )
+vector < Semivariance* > calculate_all_semivariances( vector< WindVector* > &input_data )
 {
 	vector < Semivariance* > semivariances;
 
-	for(int i = 0; i < input_data.size - 2; i++)
+	for(int i = 0; i < input_data.size() - 1; i++)
 	{
-		for(int j = i + 1; j < input_data.size - 1; j++)
+		for(int j = i + 1; j < input_data.size(); j++)
 		{
+			// variables for pythagoras
+			float x1 = input_data[ i ]->x;
+			float x2 = input_data[ j ]->x;
+			float y1 = input_data[ i ]->y;
+			float y2 = input_data[ j ]->y;
+
 			// find the displacement of the two samples using their position coordinates
-			float displacement = sqrt( pow( input_data[ i ]->x - input_data[ j ]->x, 2 ) + pow( input_data[ i ]->y - input_data[ j ]->y, 2 ) );
+			// using pythagoras
+			float displacement = sqrt( pow( x1 - x2, 2 ) + pow( y1 - y2, 2 ) );
 		
 			float gamma[4];
 
 			for( int k = 0; k < 4; k++ )
 			{
-				gamma[ k ] = 0.5 * ( pow( input_data[ i ]->velocity[ k ] - input_data[ j ]->velocity[ k ], 2 ) );
+				// variables for the semivariance equation
+				float v1 = input_data[ i ]->velocity[ k ];
+				float v2 = input_data[ j ]->velocity[ k ];
+
+				// semivariance equation for points i and j
+				gamma[ k ] = 0.5 * pow( v1 - v2, 2 );
 			}
+
+			// Create the semivariance object and add it to the list
+			Semivariance* newSemivariance = new Semivariance( displacement, gamma );
+
+			cout << "The semivariance created has displacement: " << newSemivariance->displacement << "\n";
+			semivariances.push_back( newSemivariance );
 		}
 	}
+
+	cout << "size of semivariances (in function): " << semivariances.size() << "\n";
+	return semivariances;
 }
 
 int main()
